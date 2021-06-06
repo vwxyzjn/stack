605b4102c29b7805f66c7f88df80163069904a11
Costa Huang <costa.huang@outlook.com>
2021-06-06 00:30:27 -0400

    quick fix

--- stack/dns.tf
+++ stack/dns.tf
@@ -56,7 +56,7 @@ resource "aws_route53_record" "costah_dev" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.costah_dev.zone_id
+  zone_id         = aws_route53_zone.costah_dev.zone_id
 }
 
 resource "aws_acm_certificate_validation" "costah_dev" {
